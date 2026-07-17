bdbf71e9c9bbaacc9e9cdc2ba57b52178f7a5f3c
AbdollahHosseini <unihosseini.a@gmail.com>
2026-07-17 21:28:32 +0300

    included assert tests inside of clean.py and eliminated a few insects

--- src/stocknn/data_pipeline/clean.py
+++ src/stocknn/data_pipeline/clean.py
@@ -1,4 +1,4 @@
-from stocknn.data_pipeline.ingest import TickerData, write_data_to_csv
+from stocknn.data_pipeline.ingest import write_data_to_csv
 from pathlib import Path
 import polars as pl
 
@@ -15,12 +15,20 @@ def clean_data(data):
     return data.drop_nulls()
 
 
+def test_data(data):
+    assert not data.is_empty(), "DataFrame is empty after cleaning."
+    # assert data.null_count() == 0, "DataFrame still contains null values after cleaning."
+    assert data.null_count().sum_horizontal().item() == 0, "DataFrame still contains null values after cleaning."
+    assert data.shape[0] > 0, "DataFrame has no rows after cleaning."
+    assert data.is_sorted, "Date column is not sorted in descending order."
+
 def main():
-    data = TickerData()
+    data = pl.read_csv(Path(__file__).parents[3] / "data" / "raw" / "ticker_data.csv")
     data = clean_data(data)
     output_file = Path(__file__).parents[3] / "data" / "processed" / "ticker_data.csv"
     write_data_to_csv(data, output_file)
     print(f"Data written to {output_file}")
+    test_data(data)
 
 
 if __name__ == "__main__":
